c11ecd22ebf9c7351fff4cce0f3c68d32faf0aa3
LC15 <schowdhury12@aol.com>
2015-02-08 17:04:02 -0500

    added fib

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -13,4 +13,24 @@ public class Recursion{
 	return wurk(m,a);
 
     }
+    public int fib(int n){
+	if(n==0){return 0;}
+	if(n==1){return 1;}
+	return fibHELPWORKPLS(1,n,0,1);
+	//a function that keeps track of position, where you want to be, and sum
+
+
+    }
+
+    public int fibHELPWORKPLS(int p, int n, int a, int b){
+	if(n==p){
+	    return b;}
+	else{
+	    p++;
+	    int s=a+b;
+	    a=b;
+	    b=s;
+	    return fibHELPWORKPLS(p,n,a,b);}
+
+    }
 }
