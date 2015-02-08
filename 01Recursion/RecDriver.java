64fb07d520e8af956f150c562b8eecbb826083c4
LC15 <schowdhury12@aol.com>
2015-02-08 17:02:16 -0500

    using this to test my code

---
+++ 01Recursion/RecDriver.java
@@ -0,0 +1,12 @@
+public class RecDriver{
+    public static void main(String[]args){
+	Recursion test=new Recursion();
+	System.out.println(test.fact(3)); //should print 6
+	System.out.println(test.fact(1)); //should print 1
+			   System.out.println(test.fib(0));//print 0
+			   System.out.println(test.fib(1));//print 1
+			   System.out.println(test.fib(7));//print 13
+			    
+    }
+}
+			   
