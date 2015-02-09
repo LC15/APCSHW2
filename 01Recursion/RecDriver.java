146f146ab4c206e0cadc106b88acde5dd4fcd045
LC15 <schowdhury12@aol.com>
2015-02-08 21:55:50 -0500

    done

--- 01Recursion/RecDriver.java
+++ 01Recursion/RecDriver.java
@@ -3,9 +3,12 @@ public class RecDriver{
 	Recursion test=new Recursion();
 	System.out.println(test.fact(3)); //should print 6
 	System.out.println(test.fact(1)); //should print 1
+		System.out.println(test.fact(0));//print 1
+		//	System.out.println(test.fact(-1));//illegal
 			   System.out.println(test.fib(0));//print 0
 			   System.out.println(test.fib(1));//print 1
 			   System.out.println(test.fib(7));//print 13
+			   System.out.println(test.sqrt(25.0)); //print 5
 			    
     }
 }
