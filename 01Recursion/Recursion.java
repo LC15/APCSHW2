c0f9435be385d478a9f65fc2e44b61c20854a725
LC15 <schowdhury12@aol.com>
2015-02-08 21:55:31 -0500

    done

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -3,17 +3,26 @@ public class Recursion{
 	return "Chowdhury,Labiba";
     }
     public int fact(int n){
-	if(n<0){}
+
+	if(n<0){
+	    throw new IllegalArgumentException("Enter a number greater than 0 in order to find the factorial value");}
+
+
 	return wurk(1,n);
     }
     public int wurk(int m, int a){ 
-	m=m*a;
-	if (a==1){return m;} 
+	
+	if (a==1||a==0){return m;} 
+	
+m=m*a;
 	a--;
 	return wurk(m,a);
-
+	
     }
     public int fib(int n){
+	if(n<0){
+	    throw new IllegalArgumentException("Enter a number greater than or equal to 0 in order to find the value at the entered position of the fibonacci sequence");}
+
 	if(n==0){return 0;}
 	if(n==1){return 1;}
 	return fibHELPWORKPLS(1,n,0,1);
@@ -33,4 +42,19 @@ public class Recursion{
 	    return fibHELPWORKPLS(p,n,a,b);}
 
     }
+
+    public double sqrt(double n){
+	if(n<0.0){System.out.println("enter a positive number to find the square root of that number.");
+				     //return;
+	}
+		return halpSQRT(1,n);    
+    }
+       
+	public double halpSQRT(double guess,double n){
+	    if(guess*guess==n){return guess;}
+	   double nguess=((n/guess)+guess)/2;
+	    return halpSQRT(nguess, n);
+	}
 }
+
+    
